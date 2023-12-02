e966ef016443e3628492810faa9b698b2844fbab
tengku yasin habib <tengkuyasinhabib@gmail.com>
2023-12-02 15:54:25 +0700

    finishing aplikasi

---
+++ database/migrations/2023_11_29_132654_create_menu_roles_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('menu_roles', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('menu_id');
+            $table->unsignedBigInteger('role_id');
+            $table->timestamps();
+            $table->foreign('menu_id')->references('id')->on('menus');
+            $table->foreign('role_id')->references('id')->on('roles');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('menu_roles');
+    }
+};
